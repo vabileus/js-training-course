17f1d58b44a3971b3f909749496e875befa7dc33
Vasyl Babala <babala.vasyl@gmail.com>
2020-05-21 17:42:52 +0300

    update

--- tasks/script1.js
+++ tasks/script1.js
@@ -4,9 +4,11 @@ function loopComma(str)
 { 
     const n = str.length; 
     const opsize = Math.pow(2, n - 1); 
-    let temp = '';  
+    let result = [];  
     for (let counter = 0; counter < opsize; counter++)  
     {         
+        let temp = '';
+        
         for (let j = 0; j < n; j++)  
         { 
             temp += str[j];
@@ -14,9 +16,9 @@ function loopComma(str)
             if (counter & (1 << j)) 
             temp += '.'; 
         }
-        temp += ' ';  
+        result.push(temp);  
     } 
-    //console.log(temp);
+    console.log(result);
 } 
 
 const rl = readline.createInterface({
